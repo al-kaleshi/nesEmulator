936202062d83af9ac3e163a66b3de69cf776b337
al-kaleshi <al.kaleshi00@gmail.com>
2023-11-12 18:05:35 -0600

    Final Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -68,100 +68,102 @@ export default function Home() {
   }
 
   function tick() {
-    cpu!.tick()
+    cpu!.tick();
     update();
   }
 
   return (
-    <main>
-      <label>Upload File</label>
-      <input
-        className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none"
-        id="file_input"
-        type="file"
-        onChange={handleChange}
-      ></input>
-      {cpu === undefined ? null : (
-        <div className="grid gap-4 grid-cols-2">
-          <div>
-            <h1 className="font-bold">Status Flags</h1>
-            <table>
-              <thead>
-                <tr>
-                  <th>Flag Name</th>
-                  <th>Flag Value</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Carry</td>
-                  <td>{carryFlag ? "TRUE" : "FALSE"}</td>
-                </tr>
-                <tr>
-                  <td>Zero</td>
-                  <td>{zeroFlag ? "TRUE" : "FALSE"}</td>
-                </tr>
-                <tr>
-                  <td>Interrupt Disable</td>
-                  <td>{interruptDisableFlag ? "TRUE" : "FALSE"}</td>
-                </tr>
-                <tr>
-                  <td>Decimal</td>
-                  <td>{decimalFlag ? "TRUE" : "FALSE"}</td>
-                </tr>
-                <tr>
-                  <td>Overflow</td>
-                  <td>{overflowFlag ? "TRUE" : "FALSE"}</td>
-                </tr>
-                <tr>
-                  <td>Negative</td>
-                  <td>{negativeFlag ? "TRUE" : "FALSE"}</td>
-                </tr>
-              </tbody>
-              <div className="mt-10">
-                <span>Program Counter: </span>
-                <span>{`0x${pc.toString(16)}`}</span>
-              </div>
-              <button
-                type="button"
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mt-2"
-                onClick={tick}
-              >
-                Tick
-              </button>
-            </table>
+    <main className="p-4 flex justify-center items-center h-screen">
+      <div className="border-2 shadow rounded-xl p-4">
+        <label>Upload File</label>
+        <input
+          className="block w-fit text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none"
+          id="file_input"
+          type="file"
+          onChange={handleChange}
+        ></input>
+        {cpu === undefined ? null : (
+          <div className="grid gap-4 grid-cols-[max-content_max-content] text-left">
+            <div>
+              <h1 className="font-bold">Status Flags</h1>
+              <table className="w-[300px]">
+                <thead>
+                  <tr>
+                    <th>Flag Name</th>
+                    <th>Flag Value</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Carry</td>
+                    <td>{carryFlag ? "TRUE" : "FALSE"}</td>
+                  </tr>
+                  <tr>
+                    <td>Zero</td>
+                    <td>{zeroFlag ? "TRUE" : "FALSE"}</td>
+                  </tr>
+                  <tr>
+                    <td>Interrupt Disable</td>
+                    <td>{interruptDisableFlag ? "TRUE" : "FALSE"}</td>
+                  </tr>
+                  <tr>
+                    <td>Decimal</td>
+                    <td>{decimalFlag ? "TRUE" : "FALSE"}</td>
+                  </tr>
+                  <tr>
+                    <td>Overflow</td>
+                    <td>{overflowFlag ? "TRUE" : "FALSE"}</td>
+                  </tr>
+                  <tr>
+                    <td>Negative</td>
+                    <td>{negativeFlag ? "TRUE" : "FALSE"}</td>
+                  </tr>
+                </tbody>
+                <div className="mt-10">
+                  <span>Program Counter: </span>
+                  <span>{`0x${pc.toString(16)}`}</span>
+                </div>
+                <button
+                  type="button"
+                  className="text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mt-2"
+                  onClick={tick}
+                >
+                  Tick
+                </button>
+              </table>
+            </div>
+            <div>
+              <h1 className="font-bold">Registers</h1>
+              <table className="w-[300px]">
+                <thead>
+                  <tr>
+                    <th>Register Name</th>
+                    <th>Register Value</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Accumulator</td>
+                    <td>{`0x${accumulator.toString(16)}`}</td>
+                  </tr>
+                  <tr>
+                    <td>X</td>
+                    <td>{`0x${x.toString(16)}`}</td>
+                  </tr>
+                  <tr>
+                    <td>Y</td>
+                    <td>{`0x${y.toString(16)}`}</td>
+                  </tr>
+                  <tr>
+                    <td>Stack Pointer</td>
+                    <td>{`0x${stackPointer.toString(16)}`}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
-          <div>
-            <h1 className="font-bold">Registers</h1>
-            <table>
-              <thead>
-                <tr>
-                  <th>Register Name</th>
-                  <th>Register Value</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Accumulator</td>
-                  <td>{`0x${accumulator.toString(16)}`}</td>
-                </tr>
-                <tr>
-                  <td>X</td>
-                  <td>{`0x${x.toString(16)}`}</td>
-                </tr>
-                <tr>
-                  <td>Y</td>
-                  <td>{`0x${y.toString(16)}`}</td>
-                </tr>
-                <tr>
-                  <td>Stack Pointer</td>
-                  <td>{`0x${stackPointer.toString(16)}`}</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      )}
+        )}
+      </div>
     </main>
   );
 }
